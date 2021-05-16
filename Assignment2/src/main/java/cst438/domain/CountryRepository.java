c0208af006155cbdd423c281a4eaaccb34d3e5be
tvgomez <avillagomez@csumb.edu>
2021-05-16 09:41:49 -0700

    Delete Assignment2 directory

--- Assignment2/src/main/java/cst438/domain/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package cst438.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Integer> {
-	
-	Country findByCode(String code);
-
-}
